e17457fe7ed73126202ad3736da3a0ec17fb3b01
nanoha <nanoha.creator@gmail.com>
2022-10-10 14:42:33 +0100

    add files for about page, fix humberger btn (navigation close by click everywhere)

--- js/humbergerBtn.js
+++ js/humbergerBtn.js
@@ -1,4 +1,4 @@
-/* トグルボタン制御 */
+/* ハンバーガーボタンのクリックで、メニューを開閉する */
 $(".bl_humbergerBtn").click(function () {
   /* ハンバーガーボタンのオープンを設定 */
   $(this).toggleClass("is_close");
@@ -8,14 +8,12 @@ $(".bl_humbergerBtn").click(function () {
   $("body").toggleClass("is_noscroll");
 });
 
-/* メニューのリンクが押下されたときに、ナビゲーションを隠す */
-$(".bl_headerNav_link").click(function () {
-  if ($(window).width() < 768) {
-    /* ハンバーガーボタンのクローズを設定 */
-    $(".bl_humbergerBtn").removeClass("is_close");
-    /* ナビゲーションの非表示を設定 */
-    $(".bl_headerNav").removeClass("is_fade");
-    /* スクロールの有を設定 */
-    $("body").removeClass("is_noscroll");
-  }
+/* どこかがクリックされたときに、ナビゲーションを隠す */
+$(".bl_headerNav").click(function () {
+  /* ハンバーガーボタンのクローズを設定 */
+  $(".bl_humbergerBtn").removeClass("is_close");
+  /* ナビゲーションの非表示を設定 */
+  $(".bl_headerNav").removeClass("is_fade");
+  /* スクロールの有を設定 */
+  $("body").removeClass("is_noscroll");
 });
